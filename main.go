89072c60894463900c805e4df286977987f05619
Matt Krueger <mkrueger@rstms.net>
2024-12-27 02:43:45 -0700

    Bump version: 0.0.1 → 0.0.2

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ var iconData []byte
 const serverName = "winexec"
 const defaultPort = 10080
 const SHUTDOWN_TIMEOUT = 5
-const Version = "0.0.1"
+const Version = "0.0.2"
 
 var Verbose bool
 var Debug bool
